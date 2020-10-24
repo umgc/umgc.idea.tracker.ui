0520cc05e188912a5f2cf313c2a38b47fc11ed5e
tarunlava <tarunlava@users.noreply.github.com>
2020-10-23 23:24:42 -0400

    Added basic auth with properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from './login/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'project-tracker';
+
+  isLoggedIn = false;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService) { }
+
+
+
+  handleLogout() {
+    this.authenticationService.logout();
+  }
+
 }
