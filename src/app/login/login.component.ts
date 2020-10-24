ec32cd3b5c7621a90ef743dcdb5f071052b4c815
tarunlava <tarunlava@users.noreply.github.com>
2020-10-23 23:24:42 -0400

    Added basic auth with properties

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from './auth.service';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  password : string;
+  errorMessage = 'Invalid Credentials';
+  successMessage: string;
+  invalidLogin = false;
+  loginSuccess = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private appComponent: AppComponent ,
+    private authenticationService: AuthenticationService) {   }
+    
 
   ngOnInit(): void {
   }
+
+
+  handleLogin() {
+    this.authenticationService.authenticationService(this.username, this.password).subscribe((result)=> {
+      this.invalidLogin = false;
+      this.loginSuccess = true;
+      this.successMessage = 'Login Successful.';
+      this.appComponent.isLoggedIn = this.authenticationService.isUserLoggedIn();
+      this.router.navigate(['/proposal']);
+    }, () => {
+      this.invalidLogin = true;
+      this.loginSuccess = false;
+    });      
+  }
+
 
 }
